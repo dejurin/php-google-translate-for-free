59e4b50b95546665659c8e4c4972f4eed909154f
YY <gkhelloworld@gmail.com>
2019-02-15 09:44:42 +0000

    Apply fixes from StyleCI

--- example.php
+++ example.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once ('GoogleTranslateForFree.php');
+require_once 'GoogleTranslateForFree.php';
 
 //Single
 $source = 'en';
@@ -11,13 +11,13 @@ $text = 'Hello';
 $tr = new GoogleTranslateForFree();
 $result = $tr->translate($source, $target, $text, $attempts);
 
-var_dump($result); 
+var_dump($result);
 
 //Array
 $source = 'en';
 $target = 'ru';
 $attempts = 5;
-$arr = array('hello','world');
+$arr = ['hello', 'world'];
 
 $tr = new GoogleTranslateForFree();
 $result = $tr->translate($source, $target, $arr, $attempts);
